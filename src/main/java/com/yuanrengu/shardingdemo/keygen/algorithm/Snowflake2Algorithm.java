15b0342fb0cca1cccdd55cfb1a3115410ae3ea86
yuanrengu <heyonggang0811@126.com>
2020-10-09 11:41:03 +0800

    新增3个snowflake变种算法

---
+++ src/main/java/com/yuanrengu/shardingdemo/keygen/algorithm/Snowflake2Algorithm.java
@@ -0,0 +1,107 @@
+package com.yuanrengu.shardingdemo.keygen.algorithm;
+
+/**
+ * from https://github.com/souyunku/SnowFlake
+ */
+public class Snowflake2Algorithm {
+
+    /**
+     * 起始的时间戳
+     */
+    private final static long START_STMP = 1480166465631L;
+
+    /**
+     * 每一部分占用的位数
+     */
+    private final static long SEQUENCE_BIT = 12; // 序列号占用的位数
+    private final static long MACHINE_BIT = 5;   // 机器标识占用的位数
+    private final static long DATACENTER_BIT = 5;// 数据中心占用的位数
+
+    /**
+     * 每一部分的最大值
+     */
+    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
+    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);  // 其实就是2^12-1，只不过它运行或许更快，之所以说或许是因为现代编译器遇见2的多少次方都会自动优化成左移操作。然后跟全1做异或操作，实现的就是按位取反。
+
+    /**
+     * 每一部分向左的位移
+     */
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    private long datacenterId;  // 数据中心
+    private long machineId;     // 机器标识
+    private long sequence = 0L; // 序列号
+    private long lastStmp = -1L;// 上一次时间戳
+
+    public Snowflake2Algorithm(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * 产生下一个ID
+     *
+     * @return
+     */
+    public synchronized long nextId() {
+        long currStmp = getNewstmp();
+        if (currStmp < lastStmp) {
+            throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStmp == lastStmp) {
+            // 相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            // 同一毫秒的序列数已经达到最大
+            // 如果在同一个机器（网络进程），同一个毫秒内，序号自增还越界了，那没办法再生成新序号了，只能等下一个毫秒。
+            if (sequence == 0L) {
+                currStmp = getNextMill();
+            }
+        } else {
+            // 不同毫秒内，序列号重置为0
+            sequence = 0L;
+        }
+
+        lastStmp = currStmp;
+
+        // 就是用相对毫秒数、机器ID和自增序号，3个东西，按顺序拼接成8字节的Long类型数据。
+        // 需要提醒的是这里的毫秒数是相对的，相对epoch的时间点。所以会有currStmp - START_STMP的逻辑。
+        return (currStmp - START_STMP) << TIMESTMP_LEFT // 时间戳部分
+                | datacenterId << DATACENTER_LEFT       // 数据中心部分
+                | machineId << MACHINE_LEFT             // 机器标识部分
+                | sequence;                             // 序列号部分
+    }
+
+    private long getNextMill() {
+        long mill = getNewstmp();
+        while (mill <= lastStmp) {
+            mill = getNewstmp();
+        }
+        return mill;
+    }
+
+    private long getNewstmp() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        Snowflake2Algorithm snowFlake = new Snowflake2Algorithm(2, 3);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(snowFlake.nextId());
+        }
+
+        System.out.println(System.currentTimeMillis() - start);
+
+    }
+}
